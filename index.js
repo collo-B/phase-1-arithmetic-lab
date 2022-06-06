1c903bc78e3bfa55b1ddd205d89c7a5942613fa4
BII KIPKOECH COLLINS <ckipkoech17@gmail.com>
2022-06-07 00:41:59 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random()*10);
+let num3 = 24;
+let num4 = 5;
+const mod = num3 % num4;
+let num5 = 14;
+let num6 = 20;
+let num7 = 17;
+const max = Math.max(num5, num6, num7);
+
+
+
+ 
